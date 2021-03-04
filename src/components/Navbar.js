2175c23a8c3aebbc0f685db163bca74689fc40a4
Your Name <you@example.com>
2021-03-04 03:27:30 +0100

    setting navbar toggle

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
+import logo from "../images/logo.svg";
+import { FaAlignRight } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
+  state = { isOpen: false };
+  handleToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
   render() {
     return <div>NAVbar say he is going to be errwhere</div>;
   }
