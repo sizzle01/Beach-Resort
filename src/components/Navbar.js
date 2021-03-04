e700b1c6d149a5475c2a2d9ffe15395a04aa1e58
Your Name <you@example.com>
2021-03-04 16:36:06 +0100

    navbar completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,33 @@ export default class Navbar extends Component {
     this.setState({ isOpen: !this.state.isOpen });
   };
   render() {
-    return <div>NAVbar say he is going to be errwhere</div>;
+    return (
+      <nav className="navbar">
+        <div className="nav-center">
+          <div className="nav-header">
+            <Link to="/">
+              <img src={logo} alt="resort logo" />
+            </Link>
+            <button
+              type="button"
+              className="nav-btn"
+              onClick={this.handleToggle}
+            >
+              <FaAlignRight className="nav-icon" />
+            </button>
+          </div>
+          <ul
+            className={this.state.isOpen ? "nav-links show-nav" : "nav-links"}
+          >
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/rooms">Rooms</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
