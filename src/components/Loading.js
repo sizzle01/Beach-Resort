5bec4cb7817af34a5c66d7538c03ef414cbe7b33
Your Name <you@example.com>
2021-03-18 22:58:04 +0100

    adding featured room section

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import LoadingGif from '../images/gif/loading-arrow.gif';
-export default function Loading(){
-    return(
-        <div className='loading'>
-        <h4>Rooms data loading...</h4>
-        <img src={loadingGif} alt="" />
-        </div>
-    )
+import React from "react";
+import loadingGif from "../images/gif/loading-arrow.gif";
+export default function Loading() {
+  return (
+    <div className="loading">
+      <h4>Rooms data loading...</h4>
+      <img src={loadingGif} alt="" />
+    </div>
+  );
 }
